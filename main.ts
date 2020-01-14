29510992ff5f19a40da3de55a88c25ce9ff2297f
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-14 22:05:22 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,59 @@
-basic.forever(function () {
-	
+function playTune () {
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(349, music.beat(BeatFraction.Double))
+        music.playTone(349, music.beat(BeatFraction.Double))
+    }
+    if (master == true) {
+        channel += 1
+        radio.sendString("play")
+    }
+    for (let index = 0; index < 2; index++) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Double))
+        music.playTone(440, music.beat(BeatFraction.Double))
+    }
+    if (master == true) {
+        channel += 1
+        radio.sendString("play")
+    }
+    for (let index = 0; index < 2; index++) {
+        music.playTone(523, music.beat(BeatFraction.Double))
+        music.playTone(523, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Double))
+    }
+    if (master == true) {
+        channel += 1
+        radio.sendString("play")
+    }
+    for (let index = 0; index < 2; index++) {
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(466, music.beat(BeatFraction.Whole))
+        music.playTone(440, music.beat(BeatFraction.Double))
+        music.playTone(440, music.beat(BeatFraction.Double))
+    }
+}
+radio.onReceivedString(function (receivedString) {
+    playTune()
 })
+input.onButtonPressed(Button.AB, function () {
+    if (channel == 0) {
+        master = true
+        playTune()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    channel += 1
+    radio.setGroup(channel)
+})
+input.onButtonPressed(Button.A, function () {
+    channel += -1
+    radio.setGroup(channel)
+})
+let channel = 0
+let master = false
+master = false
+channel = 0
+radio.setGroup(channel)
